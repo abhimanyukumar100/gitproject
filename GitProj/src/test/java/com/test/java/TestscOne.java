b0f47f7338a9b2a66f17e1c92b13f97f65b4ef49
abhimanyukumar100 <kumar.abhimanyu68@gmail.com>
2018-10-29 08:29:18 +0530

    First commit

---
+++ GitProj/src/test/java/com/test/java/TestscOne.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.test.java;
+
+/**
+ * @author abhii
+ *
+ */
+public class TestscOne {
+
+	/**
+	 * 
+	 */
+	public TestscOne() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
